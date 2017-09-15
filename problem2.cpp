0369d851baa780e5c6df3ed8d585f1177100f5cb
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-09-15 15:54:26 +0530

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+          float r,d,area,circum;
+          cout<<"\n Enter the radius of a circle.";
+          cin>>r;
+          d=2*r;
+          area=3.14*r*r;
+          circum=2*3.14*r;
+          cout<<"\n Diameter of the circle is="<<d<<"\n Area of the circle is="<<area<<"\n Circumference of the circle is="<<circum<<endl;
+    return 0;
+}
